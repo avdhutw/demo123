1082e2d7b85a0c3731d61387ad40f73a8e3fb615
avdhutw <avdhutwakchaure868@gmail.com>
2020-08-27 23:03:23 +0530

    First Commit Vik

---
+++ DemoMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using DemoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeEntities employeeEntities = new EmployeeEntities();
+
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            //Employee employee = new Employee();
+
+           var data =  employeeEntities.EmployeeDetails.ToList();
+
+            return View(data);
+        }
+    }
+}
---
+++ DemoMvc/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace DemoMvc.Models
+{
+    public class Employee
+    {
+
+        public int Emp_id { get; set; }
+
+        public string Emp_Name  { get; set; }
+
+        public string  Gender { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
